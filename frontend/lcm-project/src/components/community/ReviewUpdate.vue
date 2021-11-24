a4c819c77378fa3bfb760527954a460f71b47108
우동진 <wjinh93@gmail.com>
2021-11-25 01:48:56 +0900

    added star at update

--- frontend/lcm-project/src/components/community/ReviewUpdate.vue
+++ frontend/lcm-project/src/components/community/ReviewUpdate.vue
@@ -9,13 +9,57 @@
         <h4 class="movie-title">Movie : {{ data.movie.title }}</h4>
         <hr />
         <div class="title">
-          <label for="title">제목 </label>
+          <label for="title">Title: </label>
           <input v-model="data.title" type="text" />
         </div>
         <br />
-
+        <div class="ratings">
+          <span>Ratings:</span>
+          <div class="star-rating">
+            <input
+              type="radio"
+              id="5-stars"
+              name="rating"
+              value="5"
+              v-model="data.score"
+            />
+            <label for="5-stars" class="star pr-4">★</label>
+            <input
+              type="radio"
+              id="4-stars"
+              name="rating"
+              value="4"
+              v-model="data.score"
+            />
+            <label for="4-stars" class="star">★</label>
+            <input
+              type="radio"
+              id="3-stars"
+              name="rating"
+              value="3"
+              v-model="data.score"
+            />
+            <label for="3-stars" class="star">★</label>
+            <input
+              type="radio"
+              id="2-stars"
+              name="rating"
+              value="2"
+              v-model="data.score"
+            />
+            <label for="2-stars" class="star">★</label>
+            <input
+              type="radio"
+              id="1-star"
+              name="rating"
+              value="1"
+              v-model="data.score"
+            />
+            <label for="1-star" class="star">★</label>
+          </div>
+        </div>
         <div class="content">
-          <label for="content">내용</label>
+          <label for="content">Content : </label>
           <br />
           <textarea
             v-model="data.content"
@@ -47,8 +91,8 @@ export default {
         title: "",
         content: "",
         is_private: false,
+        score: null,
       },
-      content: "",
     };
   },
   created() {
